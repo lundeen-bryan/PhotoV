b2f7f9320ce55953d06129ad03876e341ce1fc44
lundeen-bryan <lundeen.bryan@gmail.com>
2021-02-08 20:06:59 -0800

    Added gallery sect w/tag select ft

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 // JavaScript Document
-$(document) ready(function(){
-    $('nav-button')click(function(){
-        $('.nav-button') toggleClass('change');
+$(document).ready(function(){
+    $('nav-button').click(function(){
+        $('.nav-button').toggleClass('change');
     })
 })
+
+/* -- Gallery tag picker -- */
+  $('.gallery-list-item').click(function() {
+    let value = $(this).attr('data-filter');
+    if(value === 'all') {
+      $('.filter').show(300);
+    } else {
+      $('.filter').not('.' + value).hide(300);
+      $('.filter').filter('.' + value).show(300);
+    }
+  });
+
+  $('.gallery-list-item').click(function() {
+    $(this).addClass('active-item').siblings().removeClass('active-item');
+  });
